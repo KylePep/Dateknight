7dfb0e37f1b093212429f182d7f5744fefb8730e
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2026-04-03 20:58:40 -0600

    fixed email jobs

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Jobs\SendWelcomeEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
